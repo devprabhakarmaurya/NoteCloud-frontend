3979f472acf4824d446558aa91159c165327b0f7
Prabhakar <techprabhakarmaurya@gmail.com>
2023-10-01 11:32:04 +0530

    Setting up app complete with home and about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is NoteCloud App</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
